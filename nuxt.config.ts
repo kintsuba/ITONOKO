29292aab8393b12bca8919d86efcb10902818219
Kintsuba <bnxth107@gmail.com>
2022-08-23 13:58:54 +0900

    feat: set config and head properly

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,27 +2,8 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  head: {
-    title: "ITONOKO",
-    ssr: true,
-    server: "static",
-
-    htmlAttrs: {
-      lang: "ja",
-    },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content:
-          "いつもオヤカタに寄りそう糸ノコちゃんをイメージした、『毎日こつこつ俺タワー』のデータベースサイトです。",
-      },
-    ],
-  },
-
+  ssr: true,
+  target: "static",
   srcDir: "src/",
-
   modules: ["nuxt-windicss", "unplugin-icons/nuxt"],
 });
--- src/app.vue
+++ src/app.vue
@@ -7,4 +7,20 @@
 
 <script setup lang="ts">
 const ItonokoHeader = resolveComponent("ItonokoHeader");
+
+useHead({
+  title: "ITONOKO",
+  viewport: "width=device-width, initial-scale=1",
+  charset: "utf-8",
+  htmlAttrs: {
+    lang: "ja",
+  },
+  meta: [
+    {
+      name: "description",
+      content:
+        "いつもオヤカタに寄りそう糸ノコちゃんをイメージした、『毎日こつこつ俺タワー』のデータベースサイトです。",
+    },
+  ],
+});
 </script>
