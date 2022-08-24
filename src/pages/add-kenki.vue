663b117cbbf21dff83e6d9ccce86ab50f2e27840
Kintsuba <bnxth107@gmail.com>
2022-08-24 17:16:43 +0900

    feat: create the add-kenki page

---
+++ src/pages/add-kenki.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="container mx-auto px-4">
+    <h1 class="text-2xl font-bold font-display my-6">建姫追加</h1>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,11 @@
     <h1 class="text-2xl font-bold font-display my-6">建姫編集</h1>
 
     <KenkiTable />
+    <ItonokoFab />
   </main>
 </template>
 
 <script setup lang="ts">
 import KenkiTable from "@/components/KenkiTable.vue";
+import ItonokoFab from "@/components/ItonokoFab.vue";
 </script>
