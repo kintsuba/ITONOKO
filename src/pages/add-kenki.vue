e6700235c6de67d169edfb596fb99341674ebb57
Kintsuba <bnxth107@gmail.com>
2022-08-25 17:45:52 +0900

    feat: start to make the add-kenki page

--- src/pages/add-kenki.vue
+++ src/pages/add-kenki.vue
@@ -1,7 +1,42 @@
 <template>
   <main class="container mx-auto px-4">
     <h1 class="text-2xl font-bold font-display my-6">建姫追加</h1>
+
+    <section class="grid grid-cols-1 justify-items-center">
+      <div
+        class="grid grid-cols-3 gap-4 w-full lg:w-5/7 xl:w-4/7 my-8 items-center"
+      >
+        <label class="font-bold mr-3">建姫名</label>
+
+        <ItonokoInput :placeholder="`糸ノコ`"></ItonokoInput>
+
+        <label class="font-bold mr-3">レアリティ</label>
+        <ItonokoSelect
+          :title="`rarity`"
+          :values="rarities"
+          v-model="selectedRarity"
+          class="col-span-2"
+        />
+
+        <label class="font-bold mr-3">種別</label>
+        <ItonokoSelect
+          :title="`type`"
+          :values="types"
+          v-model="selectedType"
+          class="col-span-2"
+        />
+      </div>
+    </section>
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ItonokoSelect from "@/components/form/ItonokoSelect.vue";
+import ItonokoInput from "@/components/form/ItonokoInput.vue";
+
+const rarities = ["R", "SR", "SSR"];
+const types = ["遊種", "兵種", "騎種", "王種", "神種"];
+
+const selectedRarity = ref<string>(rarities[0]);
+const selectedType = ref<string>(types[0]);
+</script>
