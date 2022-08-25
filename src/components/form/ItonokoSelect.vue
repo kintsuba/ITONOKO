1a603531d80c67dec04e164cbc18848dabae4771
Kintsuba <bnxth107@gmail.com>
2022-08-25 17:43:56 +0900

    feat: create the ItonokoSelect

---
+++ src/components/form/ItonokoSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <select
+    :title="title"
+    class="bg-primary-100 px-3 py-2 rounded border-primary-500 focus:border-primary-600 focus:ring-primary-600 focus-visible:outline-none"
+    v-model="modelValue"
+    @update:modelValue="
+      SelectedValue = $event;
+      $emit('update:modelValue', SelectedValue);
+    "
+  >
+    <option v-for="value in values" key="value" class="">
+      {{ value }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+  values: Array<String>,
+  modelValue: String,
+  placeholder: String,
+});
+
+const SelectedValue = ref<string>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", SelectedValue: string);
+}>();
+</script>
