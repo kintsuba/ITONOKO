f3538c42cae4fdaed22a01d2d9f60c482767e216
Kintsuba <bnxth107@gmail.com>
2022-08-26 16:01:27 +0900

    refactor: rename the pages

--- src/components/ItonokoFab.vue
+++ src/components/ItonokoFab.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     class="fixed bottom-4 right-4 px-6 h-14 bg-secondary-default rounded-2xl hover:bg-secondary-darker active:bg-secondary-dark shadow hover:shadow-md shadow transition ease-in duration-200 focus:outline-none flex items-center justify-center"
-    @click="$router.push('/add-kenki')"
+    @click="$router.push('/KenkiAdd')"
   >
     <IconAdd color="#29535E" width="24" height="24" />
     <span class="ml-1 font-bold">建姫を追加</span>
