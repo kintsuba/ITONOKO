1f376e17f20f508b4d7a1739e438ab9724244a30
Kintsuba <bnxth107@gmail.com>
2022-08-26 18:13:39 +0900

    feat: create page mockups

---
+++ src/pages/SkillAdd.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ItonokoBreadcrumb :breadcrumbs="breadcrumbs" class="ml-4 mt-4" />
+    <main class="container mx-auto px-4">
+      <h1 class="text-2xl font-bold font-display my-6">スキル追加</h1>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+const breadcrumbs = [
+  {
+    name: "建機追加",
+    to: "/KenkiAdd",
+  },
+];
+</script>
---
+++ src/pages/SkillList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ItonokoBreadcrumb :breadcrumbs="breadcrumbs" class="ml-4 mt-4" />
+    <main class="container mx-auto px-4">
+      <h1 class="text-2xl font-bold font-display my-6">スキル編集</h1>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import KenkiTable from "@/components/KenkiTable.vue";
+import ItonokoFab from "@/components/ItonokoFab.vue";
+
+const breadcrumbs = [
+  {
+    name: "建機追加",
+    to: "/KenkiAdd",
+  },
+];
+</script>
