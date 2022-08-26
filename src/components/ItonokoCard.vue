b439b6b243d968f1c63fb42b3032325c7b6f881e
Kintsuba <bnxth107@gmail.com>
2022-08-26 18:15:14 +0900

    feat: create index page menu

---
+++ src/components/ItonokoCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <nuxt-link
+    :to="to"
+    class="block p-6 max-w-sm bg-primary-100 rounded-lg border border-gray-200 shadow hover:bg-secondary-default"
+  >
+    <h5 class="mb-2 text-xl font-bold">
+      {{ title }}
+    </h5>
+    <p class="font-normal">
+      {{ description }}
+    </p>
+  </nuxt-link>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  to: String,
+  title: String,
+  description: String,
+});
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <main class="container mx-auto px-4">
+    <h1 class="text-2xl font-bold font-display my-6 flex items-center">
+      <IconHome color="#29535E" width="24" height="24" />
+      <span class="ml-1">Home</span>
+    </h1>
+
+    <section class="mt-16 mx-4">
+      <h2 class="text-xl font-bold font-display my-6">メニュー</h2>
+
+      <div class="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <ItonokoCard
+          :to="menu.to"
+          :title="menu.title"
+          :description="menu.description"
+          v-for="menu in menus"
+          key="menu.title"
+        />
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import IconHome from "~icons/material-symbols/home";
+
+import ItonokoCard from "@/components/ItonokoCard.vue";
+
+const menus = [
+  {
+    to: "/KenkiList",
+    title: "建姫編集",
+    description: "建姫を確認・修正したいとき",
+  },
+  {
+    to: "/KenkiAdd",
+    title: "建姫追加",
+    description: "新しく建姫を追加したいとき",
+  },
+  {
+    to: "/SkillList",
+    title: "スキル編集",
+    description: "スキルを確認・修正したいとき",
+  },
+  {
+    to: "/SkillAdd",
+    title: "スキル追加",
+    description: "新しくスキルを追加したいとき",
+  },
+];
+</script>
