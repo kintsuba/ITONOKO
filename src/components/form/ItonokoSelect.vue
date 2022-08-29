6b8f4fe9f0c9ec51b5d81db14d019b3507551796
Kintsuba <bnxth107@gmail.com>
2022-08-29 17:53:05 +0900

    fix: incorrect format key

--- src/components/form/ItonokoSelect.vue
+++ src/components/form/ItonokoSelect.vue
@@ -4,19 +4,19 @@
     class="bg-primary-100 px-3 py-2 rounded border-primary-500 focus:border-primary-600 focus:ring-primary-600 focus-visible:outline-none"
     v-model="SelectedValue"
   >
-    <option v-for="value in values" key="value" class="">
+    <option v-for="value in values" :key="value" class="">
       {{ value }}
     </option>
   </select>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  title: String,
-  values: Array<String>,
-  modelValue: String,
-  placeholder: String,
-});
+const props = defineProps<{
+  title: string;
+  values: string[];
+  modelValue: string;
+  placeholder: string;
+}>();
 
 const emit = defineEmits<{
   (e: "update:modelValue", SelectedValue: string);
