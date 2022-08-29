ff5691255805df7f7341af34a15277552bec156e
Kintsuba <bnxth107@gmail.com>
2022-08-29 17:53:27 +0900

    feat: create the buttons

---
+++ src/components/form/button/FilledButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button
+    class="bg-primary-500 hover:bg-primary-700 text-primary-100 font-bold py-2 px-6 rounded-full"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts"></script>
