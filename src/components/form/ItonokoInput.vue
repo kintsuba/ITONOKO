87421b8d0bc5c9d5b85973298740319ec5b4e187
Kintsuba <bnxth107@gmail.com>
2022-08-30 11:55:13 +0900

    fix: Adjust placeholder color to match other elements

--- src/components/form/ItonokoInput.vue
+++ src/components/form/ItonokoInput.vue
@@ -1,7 +1,7 @@
 <template>
   <input
     type="text"
-    class="rounded bg-primary-100 border-primary-500 focus:border-primary-600 focus:ring-primary-600 focus-visible:outline-none"
+    class="rounded bg-primary-100 border-primary-500 focus:border-primary-600 focus:ring-primary-600 focus-visible:outline-none placeholder-gray-400"
     :placeholder="placeholder"
     v-model="inputedValue"
   />
