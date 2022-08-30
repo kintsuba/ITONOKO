abd51ed19fe8c4143219b7a1280f5c005945c602
Kintsuba <bnxth107@gmail.com>
2022-08-30 16:04:42 +0900

    feat: add FAB to skills/list

--- src/pages/skill/list.vue
+++ src/pages/skill/list.vue
@@ -5,12 +5,18 @@
       <h1 class="text-2xl font-bold font-display my-6">スキル編集</h1>
       <SkillTable />
     </main>
+    <ExtendedFab @click="$router.push('/skill/add')">
+      <IconAdd color="#29535E" width="24" height="24" />
+      <span class="ml-1 font-bold">スキル追加</span>
+    </ExtendedFab>
   </div>
 </template>
 
 <script setup lang="ts">
+import IconAdd from "~icons/material-symbols/add";
+
 const SkillTable = resolveComponent("SkillTable");
-const ItonokoFab = resolveComponent("form/button/ExtendedFab");
+const ExtendedFab = resolveComponent("form/button/ExtendedFab");
 
 const breadcrumbs = [
   {
